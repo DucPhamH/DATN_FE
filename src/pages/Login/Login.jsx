cc479e84263beb77b15e51c514bbd0fd68d86cd9
DucPhamH <ducphamhong2@gmail.com>
2023-09-24 10:06:59 +0700

    fix css login, register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,23 +8,25 @@ export default function Login() {
       //   onSubmit={onSubmit}
       noValidate
     >
-      <h1 className='my-6 pb-10'>
-        <div className='w-auto h-3 sm:h-4 inline-flex text-4xl lg:text-red-700 font-bold'>Login</div>
+      <h1 className='my-6'>
+        <div className='w-auto h-3 sm:h-4 inline-flex text-4xl lg:text-red-700 font-bold'>Đăng nhập</div>
       </h1>
-      <div className='pb-2'>
+      <div className='pb-2 flex flex-col justify-start'>
+        <label className='text-red-900 text-left italic'> Your email</label>
         <input
           type='email'
           name='email'
           id='email'
           placeholder='Email'
-          className='block w-full p-3  text-black text-lg border border-gray-300 rounded-lg '
+          className='block w-full px-3 py-2  text-black text-lg border border-gray-300 rounded-lg '
           //   {...register('email')}
         />
-        <div className='mt-1 flex min-h-[1.5rem] text-red-600'> {/* {errors.email?.message} */}</div>
+        <div className='flex min-h-[1rem] text-xs text-red-600'> {/* {errors.email?.message} */}</div>
       </div>
-      <div className='pb-2'>
+      <div className='pb-2 flex flex-col justify-start'>
+        <label className='text-red-900 text-left italic'> Your password</label>
         <input
-          className='block w-full p-3 text-black text-lg border border-gray-300 rounded-lg'
+          className='block w-full px-3 py-2 text-black text-lg border border-gray-300 rounded-lg'
           type='password'
           name='password'
           id='password'
@@ -32,18 +34,33 @@ export default function Login() {
           autoComplete='on'
           //   {...register('password')}
         />
-        <div className='mt-1 flex min-h-[1.5rem] text-red-600'> {/* {errors.email?.message} */}</div>
+        <div className='flex min-h-[1rem] text-xs text-red-600'> {/* {errors.email?.message} */}</div>
       </div>
-      <div className='text-right text-gray-500 flex  '>
-        <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
-        <Link className='ml-1 text-red-400 hover:underline hover:text-red-700' to='/register'>
-          Đăng ký
+      <div className='text-right text-sm'>
+        <Link className='ml-1 text-blue-700 hover:underline hover:text-red-700' to='/forgot-password'>
+          Quên mật khẩu ?
         </Link>
       </div>
+
       <div className='px-4 pb-4 rounded-full pt-4'>
-        <button className='uppercase block w-full p-2 mt-3 text-lg rounded-full bg-orange-500 hover:bg-orange-600 focus:outline-none'>
+        <button className='uppercase block w-full p-2 transition-all duration-500 mt-3 text-lg rounded-full bg-orange-500 hover:bg-orange-600 focus:outline-none'>
           sign in
         </button>
+        <button className='px-4 py-3 mt-4 border flex justify-center items-center gap-2 border-slate-200 rounded-full w-full text-gray-600 font-semiboldhover:border-slate-400 hover:text-red-600 hover:shadow transition duration-150'>
+          <img
+            className='w-6 h-6'
+            src='https://www.svgrepo.com/show/475656/google-color.svg'
+            loading='lazy'
+            alt='google logo'
+          />
+          <span>Login with Google</span>
+        </button>
+        <div className='text-gray-500 flex justify-center items-center mt-2 '>
+          <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+          <Link className='ml-1 text-red-400 hover:underline hover:text-red-700' to='/register'>
+            Đăng ký
+          </Link>
+        </div>
       </div>
     </form>
   )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -8,49 +8,52 @@ export default function Register() {
       //   onSubmit={onSubmit}
       noValidate
     >
-      <h1 className='my-6 pb-10'>
-        <div className='w-auto h-3 sm:h-4 inline-flex text-4xl lg:text-red-700 font-bold'>Register</div>
+      <h1 className='my-6'>
+        <div className='w-auto h-3 sm:h-4 inline-flex text-4xl lg:text-red-700 font-bold'>Đăng kí</div>
       </h1>
-      <div className='pb-2'>
+      <div className='pb-2 flex flex-col justify-start'>
+        <label className='text-red-900 text-left italic '> Your Email</label>
         <input
           type='email'
           id='email'
           placeholder='Email'
-          className='block w-full p-3 text-lg text-black border border-gray-300 rounded-lg '
+          className='block w-full px-3 py-2 text-lg text-black border border-gray-300 rounded-lg '
           //   {...register('email')}
         />
-        <div className='mt-1 flex min-h-[1.5rem] text-red-600'> {/* {errors.email?.message} */}</div>
+        <div className='flex min-h-[1rem] text-xs text-red-600'> {/* {errors.email?.message} */}</div>
       </div>
-      <div className='pb-2'>
+      <div className='pb-2 flex flex-col justify-start'>
+        <label className='text-red-900 text-left italic'> Your password</label>
         <input
-          className='block w-full p-3 text-lg text-black border border-gray-300 rounded-lg'
+          className='block w-full px-3 py-2 text-lg text-black border border-gray-300 rounded-lg'
           type='password'
           placeholder='Password'
           autoComplete='on'
           //   {...register('password')}
         />
-        <div className='mt-1 flex min-h-[1.5rem] text-red-600'> {/* {errors.email?.message} */}</div>
+        <div className='flex min-h-[1rem] text-xs 9'> {/* {errors.email?.message} */}</div>
       </div>
-      <div className='pb-2'>
+      <div className='pb-2 flex flex-col justify-start'>
+        <label className='text-red-900 text-left italic'> Your confirm password</label>
         <input
-          className='block w-full p-3 text-lg text-black border border-gray-300 rounded-lg'
+          className='block w-full px-3 py-2 text-lg text-black border border-gray-300 rounded-lg'
           type='password'
           placeholder='Confirm Password'
           autoComplete='on'
           //   {...register('confirm_password')}
         />
-        <div className='mt-1 flex min-h-[1.5rem] text-red-600'> {/* {errors.email?.message} */}</div>
-      </div>
-      <div className='text-right text-gray-500 flex  '>
-        <span className='text-gray-400'>Bạn đã có tài khoản?</span>
-        <Link className='ml-1 text-red-400 hover:underline hover:text-red-700' to='/login'>
-          Đăng nhập
-        </Link>
+        <div className='flex min-h-[1rem] text-xs text-red-600'> {/* {errors.email?.message} */}</div>
       </div>
       <div className='px-4 pb-4 rounded-full pt-4'>
-        <button className='uppercase block w-full p-2 mt-3 text-lg rounded-full bg-orange-500 hover:bg-orange-600 focus:outline-none'>
+        <button className='uppercase block w-full transition-all duration-500 p-2 mt-3 text-lg rounded-full bg-orange-500 hover:bg-orange-600 focus:outline-none'>
           sign up
         </button>
+        <div className='text-gray-500 flex justify-center items-center mt-2 '>
+          <span className='text-gray-400'>Bạn đã có tài khoản?</span>
+          <Link className='ml-1 text-red-400 hover:underline hover:text-red-700' to='/login'>
+            Đăng nhập
+          </Link>
+        </div>
       </div>
     </form>
   )
