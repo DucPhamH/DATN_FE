42113c2263796a334e4875faa80abdb2282ae1cd
DucPhamH <ducphamhong2@gmail.com>
2023-09-25 16:03:15 +0700

    fix

--- src/components/GlobalComponents/SideBar/SideBar.jsx
+++ src/components/GlobalComponents/SideBar/SideBar.jsx
@@ -13,7 +13,7 @@ import { RiBuilding3Line } from 'react-icons/ri'
 import { useMediaQuery } from 'react-responsive'
 import { MdMenu } from 'react-icons/md'
 import { NavLink, useLocation, useRoutes } from 'react-router-dom'
-import Submenu from './Submenu'
+import Submenu from './SubMenu'
 
 export default function SideBar() {
   let isTabletMid = useMediaQuery({ query: '(max-width: 768px)' })
