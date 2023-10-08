179af168b6fb3d5fd2fbb22e39fff9b6f2d09c3e
DucPhamH <ducphamhong2@gmail.com>
2023-10-08 15:49:44 +0700

    reposive

--- src/components/LandingComponents/HeaderLanding/HeaderLanding.jsx
+++ src/components/LandingComponents/HeaderLanding/HeaderLanding.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { navBars } from '../../../services/objectUi'
 import ButtonLanding from '../ButtonLanding'
 import UserAvatarLanding from '../UserAvatarLanding'
 import DarkMode from '../../GlobalComponents/DarkMode'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { useMediaQuery } from 'react-responsive'
 
 export default function HeaderLanding() {
   const [navBar, setNavBar] = useState(false)
-  // const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
-  // console.log(theme)
+  const [activeMenu, setActiveMenu] = useState(false)
+
+  let isTabletMid = useMediaQuery({ query: '(min-width: 1023px)' })
+
+  useEffect(() => {
+    if (isTabletMid) {
+      setActiveMenu(false)
+    }
+  }, [isTabletMid])
+
+  const handleActiveMenu = () => {
+    setActiveMenu(true)
+  }
+
+  const handleCloseMenu = () => {
+    setActiveMenu(false)
+  }
 
   const changeBackground = () => {
     if (window.scrollY > 100) {
@@ -24,11 +40,37 @@ export default function HeaderLanding() {
           ${navBar ? 'bg-white/90 dark:bg-[#1c1821]/90 backdrop-filter shadow-lg' : ''}`}
     >
       <div
-        // onClick={handleActiveMenu}
+        onClick={handleActiveMenu}
         className='lg:hidden relative p-2 flex justify-center items-center z-30 focus:outline-none transform active:scale-75 transition-transform'
       >
         <AiOutlineMenu className='text-2xl' />
       </div>
+      {activeMenu && (
+        <div className='w-full absolute h-screen top-0 right-0 z-[999] dark:bg-black/90 bg-white/90'>
+          <div className='w-full relative'>
+            <ul className='navbar-links pt-24 flex flex-col justify-center items-center '>
+              {navBars.map((nav) => {
+                return (
+                  <li key={nav.id} onClick={handleCloseMenu} className='my-8 list-none mx-3'>
+                    <a
+                      className='text-gray-900 dark:text-white font-medium text-xl no-underline tracking-wide cursor-pointer dark:hover:bg-gradient-to-r dark:from-[#ef571a] dark:to-[#b11804] hover:bg-gradient-to-r from-[#a5c233] to-[#2f8c07] hover:text-white hover:px-5 hover:py-3 hover:text-xl'
+                      href={nav.path}
+                    >
+                      {nav.name}
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+            <div
+              onClick={handleCloseMenu}
+              className='absolute right-3 rounded-full transition-all duration-300 bg-gray-200 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-500 flex justify-center items-center  top-5 h-8 w-8 dark:text-2xl dark:text-yellow-400 font-extrabold dark:bg-gray-700'
+            >
+              <AiOutlineClose />
+            </div>
+          </div>
+        </div>
+      )}
 
       <h4 className='dark:text-white text-gray-900 flex justify-center items-center font-black cursor-pointer text-2xl md:text-3xl'>
         <span className='text-red-500'>Cook</span>Healthy
@@ -36,19 +78,18 @@ export default function HeaderLanding() {
       </h4>
 
       <ul className='navbar-links hidden lg:flex lg:justify-center lg:items-center lg:ml-64'>
-        <li className='list-none '>
-          {navBars.map((nav) => {
-            return (
+        {navBars.map((nav) => {
+          return (
+            <li key={nav.id} className='mx-10 list-none my-2'>
               <a
-                key={nav.id}
-                className='text-gray-900 dark:text-white font-medium text-lg mr-16 my-2 no-underline tracking-wide cursor-pointer dark:hover:bg-gradient-to-r dark:from-[#ef571a] dark:to-[#b11804] hover:bg-gradient-to-r from-[#a5c233] to-[#2f8c07] hover:text-white hover:px-5 hover:py-3 hover:text-xl'
+                className='text-gray-900 dark:text-white font-medium text-xl  no-underline tracking-wide cursor-pointer dark:hover:bg-gradient-to-r dark:from-[#ef571a] dark:to-[#b11804] hover:bg-gradient-to-r from-[#a5c233] to-[#2f8c07] hover:text-white hover:px-4 hover:py-3 hover:text-xl'
                 href={nav.path}
               >
                 {nav.name}
               </a>
-            )
-          })}
-        </li>
+            </li>
+          )
+        })}
       </ul>
       {/* <div>
         <ButtonLanding
--- src/components/LandingComponents/UserAvatarLanding/UserAvatarLanding.jsx
+++ src/components/LandingComponents/UserAvatarLanding/UserAvatarLanding.jsx
@@ -47,7 +47,7 @@ export default function UserAvatarLanding({ navBar }) {
             animate={{ opacity: 1, y: '0%' }}
             exit={{ opacity: 0, y: '-10%', transition: { duration: '0.2' } }}
             transition={{ type: 'spring', stiffness: '200', duration: '0.5' }}
-            className='z-50 absolute top-20 right-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-color-primary dark:divide-gray-600'
+            className='z-50 absolute top-16 right-6 md:top-20 md:right-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-color-primary dark:divide-gray-600'
           >
             <div className='z-50 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-color-primary dark:divide-gray-600'>
               <div className='px-4 py-3 text-gray-900 dark:text-white'>
