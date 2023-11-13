e8805e82cc0b0a7a3f9b2d04e9603cac26456932
DucPhamH <ducphamhong2@gmail.com>
2023-11-13 18:45:16 +0700

    search card

--- src/components/GlobalComponents/Header/UserAvatar/UserAvatar.jsx
+++ src/components/GlobalComponents/Header/UserAvatar/UserAvatar.jsx
@@ -22,7 +22,7 @@ export default function UserAvatar() {
     <div ref={ref}>
       <button
         className='flex  relative items-center transition-all duration-700 text-lg py-1 px-1 font-medium text-gray-900 rounded-full
-         hover:text-red-600 dark:hover:text-red-600 md:mr-0  dark:text-white'
+         hover:text-red-600 dark:hover:text-red-600 dark:text-white'
         type='button'
         onClick={() => setIsMenu(!isMenu)}
       >
