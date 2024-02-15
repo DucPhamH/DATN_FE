959f78da0224ecfa77c5dd2f812727542f4742eb
DucPhamH <ducphamhong2@gmail.com>
2024-02-15 20:05:45 +0700

    fix css

--- src/components/ProfileComponents/ModalUploadAvatar/ModalUploadAvatar.jsx
+++ src/components/ProfileComponents/ModalUploadAvatar/ModalUploadAvatar.jsx
@@ -23,31 +23,25 @@ export default function ModalUploadAvatar({ closeModalAvatar }) {
     //ghep api o day
   }
   return (
-    <div className='modal '>
-      <div className='overlay' onClick={() => closeModalAvatar()}></div>
+    <div className='modal-customs '>
+      <div className='overlay-customs' onClick={() => closeModalAvatar()}></div>
       <div className='modal-content min-w-[360px] md:min-w-[400px] dark:bg-gray-900 bg-[#f7f1db]'>
         <div className='relative w-full max-w-md max-h-full'>
-          <div
-            onClick={() => closeModalAvatar()}
-            className='absolute right-0 top-[-0.5rem] rounded-full transition-all duration-300  cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-500 flex justify-center items-center h-8 w-8  dark:text-yellow-400 font-extrabold'
-          >
-            <AiOutlineClose />
-          </div>
-          <div className='p-6 text-center'>
-            <h3 className='mb-6 font-medium text-xl md:text-2xl text-black dark:text-gray-200'>Chọn ảnh đại diện</h3>
+          <div className='px-2 text-center'>
+            <h3 className='mb-6 font-medium text-lg md:text-xl text-black dark:text-gray-200'>Chọn ảnh đại diện</h3>
             <section className='w-full mx-auto items-center '>
               <div className='max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden items-center'>
                 <div className='px-4 py-6'>
                   <div className='flex justify-center items-center' onClick={handleImageClick}>
                     {image ? (
                       <img
-                        className='h-40 border border-red-600 mb-5 w-40 rounded-full  object-contain'
+                        className='h-[16rem] w-[16rem] border border-red-600 mb-5  rounded-full  object-contain'
                         src={URL.createObjectURL(image)}
                         alt='avatar'
                       />
                     ) : (
-                      <div className='max-w-sm px-4 py-10 mb-4  bg-gray-100 border-dashed border-2 border-gray-400 rounded-full items-center mx-auto text-center cursor-pointer'>
-                        <label className='cursor-pointer'>
+                      <div className='max-w-sm h-[16rem] w-[16rem] flex justify-center mb-4  bg-gray-100 border-dashed border-2 border-gray-400 rounded-full items-center mx-auto text-center cursor-pointer'>
+                        <label id='images' className='cursor-pointer'>
                           <svg
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
@@ -64,24 +58,28 @@ export default function ModalUploadAvatar({ closeModalAvatar }) {
                           </svg>
                           <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-700'>Upload picture</h5>
                           <p className='font-normal text-sm text-gray-400 md:px-6'>
-                            Choose photo size should be less than <b className='text-gray-600'>2mb</b>
-                          </p>
-                          <p className='font-normal text-sm text-gray-400 md:px-6'>
-                            and should be in <b className='text-gray-600'>JPG, PNG</b> format.
+                            Chọn 1 ảnh theo định dạng <b className='text-gray-600'>JPG, PNG</b>.
                           </p>
                         </label>
                       </div>
                     )}
                   </div>
 
-                  <input ref={inputRef} onChange={handleImageChange} type='file' className='hidden' accept='image/*' />
+                  <input
+                    id='images'
+                    ref={inputRef}
+                    onChange={handleImageChange}
+                    type='file'
+                    className='hidden'
+                    accept='image/*'
+                  />
                   <div className='flex items-center justify-center'>
                     <div className='w-full'>
                       <label
                         onClick={handleUpload}
                         className='w-full transition-all duration-300 text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer'
                       >
-                        <button className='text-center ml-2'>Upload</button>
+                        <button className='text-center ml-2'>Tải lên</button>
                       </label>
                     </div>
                   </div>
