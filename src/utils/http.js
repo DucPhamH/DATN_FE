a1f4ee7df076c50ae080f0e0a5a23cacfbf2875a
DucPhamH <ducphamhong2@gmail.com>
2024-05-12 22:18:21 +0700

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -13,7 +13,7 @@ import { isAxiosExpiredTokenError, isAxiosUnauthorizedError } from './utils'
 
 const URL = {
   BASE_URL: 'http://localhost:4000/api',
-  DEPLOY_URL: ''
+  DEPLOY_URL: 'https://datn-be-kwjk.onrender.com/api'
 }
 class Http {
   constructor() {
@@ -21,7 +21,7 @@ class Http {
     this.refreshToken = getRefreshTokenFromLS()
     this.refreshTokenRequest = null
     this.instance = axios.create({
-      baseURL: `${URL.BASE_URL}`,
+      baseURL: `${URL.DEPLOY_URL}`,
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json'
